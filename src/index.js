0ea50f405e56b16b48d453fa30f8280d80ee8339
Aleksey Razbakov <aleksey@razbakov.com>
2019-02-01 18:21:04 +0100

    Fix install

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
+import Vue from 'vue';
 import CrudTable from './components/CrudTable.vue';
 import CrudForm from './components/CrudForm.vue';
 import CrudFormGroup from './components/CrudFormGroup.vue';
 
-export function install(Vue) {
-  if (install.installed) {
-    return;
-  }
-
-  install.installed = true;
-  Vue.component('CrudTable', CrudTable);
-  Vue.component('CrudForm', CrudForm);
-  Vue.component('CrudFormGroup', CrudFormGroup);
-}
-
-const plugin = {
-  install,
-};
-
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin);
-}
-
-export default {
+const Components = {
   CrudTable,
   CrudForm,
   CrudFormGroup,
 };
+
+Object.keys(Components).forEach((name) => {
+  Vue.component(name, Components[name]);
+});
+
+export default Components;
